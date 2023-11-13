167dfab9a9151c1b4082ea052f32e3d030917e77
pandroe <daffapandu2304@gmail.com>
2023-11-13 22:08:31 +0700

    search hadith result screen

--- lib/views/search_hadith_screen/views/search_hadith_screen.dart
+++ lib/views/search_hadith_screen/views/search_hadith_screen.dart
@@ -2,7 +2,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:haditsku/models/hadits_model.dart';
-import 'package:haditsku/utils/constant.dart';
+
+import '../../search_result_hadith_screen/views/search_result_hadith_screen.dart';
 
 class SearchHaditsScreen extends StatefulWidget {
   @override
@@ -22,12 +23,12 @@ class _SearchHaditsScreenState extends State<SearchHaditsScreen> {
       'content_of_bukhari',
       'content_of_muslim',
       'content_of_abu_daud',
-      'content_of_ahmad',
-      'content_of_darimi',
+      'content_of_nasai',
+      'content_of_tirmidzi',
       'content_of_ibnu_majah',
       'content_of_malik',
-      'content_of_nasai',
-      'content_of_tirmidzi'
+      'content_of_ahmad',
+      'content_of_darimi'
     ]).then((models) {
       setState(() {
         haditsModels = models;
@@ -53,57 +54,41 @@ class _SearchHaditsScreenState extends State<SearchHaditsScreen> {
   }
 
   void _performSearch(String query) {
-    if (query.isEmpty) {
-      setState(() {
-        searchResults.clear();
-      });
-      return;
-    }
-
+  if (query.isEmpty) {
     setState(() {
-      searchResults = allHadits.where((hadits) {
-        return hadits.arab.toLowerCase().contains(query.toLowerCase()) ||
-            hadits.id.toLowerCase().contains(query.toLowerCase());
-      }).toList();
+      searchResults.clear();
     });
+    return;
   }
 
-  TextSpan _highlightText(String text, String query) {
-    final startIndex = text.toLowerCase().indexOf(query.toLowerCase());
-    if (startIndex == -1) {
-      return TextSpan(
-        text: text,
-        style: TextStyle(
-          color: Colors.black,
+  setState(() {
+    searchResults = allHadits.where((hadits) {
+      return hadits.arab.toLowerCase().contains(query.toLowerCase()) ||
+          hadits.id.toLowerCase().contains(query.toLowerCase());
+    }).toList();
+  });
+
+  if (searchResults.isNotEmpty) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SearchResultHadithScreen(
+          searchResults: searchResults,
+          query: query,
+          haditsModels: haditsModels, // Pass haditsModels here
         ),
-      );
-    }
-
-    final endIndex = startIndex + query.length;
-    final coloredText = TextSpan(
-      text: text.substring(startIndex, endIndex),
-      style: TextStyle(
-        color: Colors.yellow,
-        fontWeight: FontWeight.bold,
       ),
-      children: [
-        _highlightText(text.substring(endIndex), query),
-      ],
-    );
-
-    return TextSpan(
-      text: text.substring(0, startIndex),
-      style: TextStyle(color: Colors.black),
-      children: [coloredText],
     );
   }
+}
 
+  
   @override
   Widget build(BuildContext context) {
-    Constant constant = Constant(context);
     return Scaffold(
       body: Column(
         children: [
+          SizedBox(height: 50,),
           TextField(
             controller: _searchController,
             decoration: InputDecoration(
@@ -114,52 +99,7 @@ class _SearchHaditsScreenState extends State<SearchHaditsScreen> {
               _performSearch(query);
             },
           ),
-          Expanded(
-            child: haditsModels.isEmpty
-                ? Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : searchResults.isEmpty
-                    ? Center(
-                        child: Text('No data available.'),
-                      )
-                    : ListView.builder(
-                        itemCount: searchResults.isNotEmpty
-                            ? searchResults.length
-                            : allHadits.length,
-                        itemBuilder: (context, index) {
-                          final hadits = searchResults.isNotEmpty
-                              ? searchResults[index]
-                              : allHadits[index];
-
-                          HaditsModel? currentHaditsModel;
-                          for (var model in haditsModels) {
-                            if (model.data.hadiths.contains(hadits)) {
-                              currentHaditsModel = model;
-                              break;
-                            }
-                          }
-
-                          return Column(
-                            children: [
-                              SizedBox(height: constant.size.height * 0.020),
-                              RichText(
-                                text: _highlightText(
-                                    hadits.arab, _searchController.text),
-                              ),
-                              SizedBox(height: constant.size.height * 0.010),
-                              RichText(
-                                text: _highlightText(
-                                  '${hadits.id} (${currentHaditsModel!.data.name} : ${hadits.number})',
-                                  _searchController.text,
-                                ),
-                              ),
-                              SizedBox(height: constant.size.height * 0.015),
-                            ],
-                          );
-                        },
-                      ),
-          ),
+         
         ],
       ),
     );
---
+++ lib/views/search_result_hadith_screen/views/search_result_hadith_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:haditsku/models/hadits_model.dart';
+import 'package:haditsku/views/search_hadith_screen/views/search_hadith_screen.dart';
+
+class SearchResultHadithScreen extends StatelessWidget {
+  final List<Hadith> searchResults;
+  final String query;
+  final List<HaditsModel> haditsModels; // Add this line
+
+  SearchResultHadithScreen({
+    required this.searchResults,
+    required this.query,
+    required this.haditsModels, // Add this line
+  });
+
+  TextSpan _highlightText(String text, String query) {
+    final startIndex = text.toLowerCase().indexOf(query.toLowerCase());
+    if (startIndex == -1) {
+      return TextSpan(
+        text: text,
+        style: TextStyle(
+          color: Colors.black,
+        ),
+      );
+    }
+
+    final endIndex = startIndex + query.length;
+    final coloredText = TextSpan(
+      text: text.substring(startIndex, endIndex),
+      style: TextStyle(
+        color: Colors.green,
+        fontWeight: FontWeight.bold,
+      ),
+      children: [
+        _highlightText(text.substring(endIndex), query),
+      ],
+    );
+
+    return TextSpan(
+      text: text.substring(0, startIndex),
+      style: TextStyle(color: Colors.black),
+      children: [coloredText],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(onPressed: () {
+          Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => SearchHaditsScreen(),), (route) => false);
+        },icon: Icon(Icons.arrow_back_ios_new, color: Colors.black)),
+        backgroundColor: Colors.transparent ,
+        elevation: 0,
+        title: Text('Search Results', style: TextStyle(color: Colors.black),),
+      ),
+      body: ListView.builder(
+        itemCount: searchResults.length,
+        itemBuilder: (context, index) {
+          final hadits = searchResults[index];
+           
+ HaditsModel? currentHaditsModel;
+                          for (var model in haditsModels) {
+                            if (model.data.hadiths.contains(hadits)) {
+                              currentHaditsModel = model;
+                              break;
+                            }
+                          }
+          return Column(
+            children: [
+              SizedBox(height: 20.0),
+              RichText(
+                text: _highlightText(hadits.arab, query),
+                 textAlign: TextAlign.justify,
+              ),
+              SizedBox(height: 10.0),
+              RichText(
+                text: _highlightText(
+                                  '${hadits.id} (${currentHaditsModel!.data.name} : ${hadits.number})',
+                  query,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              SizedBox(height: 15.0),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
